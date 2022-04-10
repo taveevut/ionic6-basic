ca13402b725e113db64edfb2ed8eb4908b2df6b9
taveeuvt <nakomah.web@gmail.com>
2022-04-10 15:12:49 +0700

    สร้างหน้าโดยไม่ใช้ components ของ ionic

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { AccountPageRoutingModule } from './account-routing.module';
+import {AccountPageRoutingModule} from './account-routing.module';
 
-import { AccountPage } from './account.page';
+import {AccountPage} from './account.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AccountPageRoutingModule
+    AccountPageRoutingModule,
+    FontAwesomeModule
   ],
   declarations: [AccountPage]
 })
-export class AccountPageModule {}
+export class AccountPageModule { }
