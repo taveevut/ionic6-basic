9d211d417b2503d74d13968d98c86db9fed83d03
taveeuvt <nakomah.web@gmail.com>
2022-04-16 14:58:10 +0700

    เพิ่ม ifram ใน Youtube

--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { YoutubePageRoutingModule } from './youtube-routing.module';
 
 import { YoutubePage } from './youtube.page';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { YoutubePage } from './youtube.page';
     IonicModule,
     YoutubePageRoutingModule
   ],
-  declarations: [YoutubePage]
+  declarations: [YoutubePage, SafePipe]
 })
 export class YoutubePageModule {}
--- src/app/youtube/youtube.page.ts
+++ src/app/youtube/youtube.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-youtube',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./youtube.page.scss'],
 })
 export class YoutubePage implements OnInit {
-
+  url: string;
   constructor() { }
 
   ngOnInit() {
+    this.url = 'https://www.youtube.com/embed/WNKGj-kHXKg';
   }
 
 }
