03bd6e6480d6f533424922a899cd18148d99f058
Taveevut Nakomah <nakomah.web@gmail.com>
2022-04-17 10:30:27 +0700

    ปรับปรุงหน้าฟอร์ม register ให้สามารถบันทึกข้อมูลได้

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AlertController, LoadingController} from '@ionic/angular';
+import {finalize} from 'rxjs/operators';
+import {RegisterService} from './services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -12,6 +15,9 @@ export class RegisterPage implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private service: RegisterService,
+    public loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
   ) {
 
     this.form = this.fb.group({
@@ -19,7 +25,7 @@ export class RegisterPage implements OnInit {
       password: ['12345678', Validators.required],
       name: ['Taveevut Nakomah', Validators.required],
       job: ['Full stack-developer', Validators.required],
-      address: ['Mauritania', [Validators.required, Validators.minLength(15)]],
+      address: ['Mauritania', [Validators.required, Validators.minLength(5)]],
     });
 
   }
@@ -27,4 +33,25 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
+  async submit() {
+    const loading = await this.loadingCtrl.create({
+      message: 'บันทึกข้อมูล...',
+    });
+    await loading.present();
+
+    if (this.form.valid) {
+      this.service.save(this.form.value)
+        .pipe(finalize(() => loading.dismiss()))
+        .subscribe(async (res) => {
+          console.log(res);
+          const alert = await this.alertCtrl.create({
+            header: 'สำเร็จ',
+            message: 'ระบบทำการบันทึกข้อมูลได้สำเร็จ',
+            buttons: ['ตกลง'],
+          });
+
+          await alert.present();
+        });
+    }
+  }
 }
