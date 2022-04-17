fb81ee9d274a2908d7438f09b597fd8ea33d9c9a
Taveevut Nakomah <nakomah.web@gmail.com>
2022-04-17 10:29:26 +0700

    เตรียมทรัพยากรก่อนเชื่อม API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appUrl: 'https://dcea-2403-6200-8883-8d20-e9b5-9dea-1988-5477.ngrok.io/api',
+  assets: 'https://dcea-2403-6200-8883-8d20-e9b5-9dea-1988-5477.ngrok.io/api/assets'
 };
