452180d2ae07fc5a8eb587459e827877302053b9
Taveevut Nakomah <nakomah.web@gmail.com>
2022-04-18 11:17:19 +0700

    ปรับปรุง todos

--- src/app/todos/todos.page.ts
+++ src/app/todos/todos.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AlertController, IonItemSliding} from '@ionic/angular';
+
+interface Item {
+  title: string;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosPage implements OnInit {
 
-  constructor() { }
+  public items: Item[];
+
+  constructor(private alertController: AlertController) {
+    this.items = [
+      {title: 'spell shallow dog', done: true},
+      {title: 'bell gather anybody', done: true},
+      {title: 'silent spring small', done: true},
+      {title: 'ancient built perfectly', done: true},
+      {title: 'signal duck cake', done: false},
+      {title: 'courage four calm', done: true},
+    ];
+  }
 
   ngOnInit() {
   }
 
+  async create() {
+    const alert = await this.alertController.create({
+      header: 'สร้างรายการ',
+      inputs: [{
+        name: 'task',
+        placeholder: 'ระบุข้อความ'
+      }],
+      buttons: [{
+        text: 'ยกเลิก', role: 'cancel'
+      },
+      {
+        text: 'สร้าง',
+        handler: data => {
+          if (data.task !== '') {
+            this.items.push({
+              title: data.task,
+              done: false
+            });
+          }
+          return data.task;
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
+
+  async edit(item: any, slidingItem: IonItemSliding) {
+    const alert = await this.alertController.create({
+      header: 'แก้ไขรายการ',
+      inputs: [{
+        name: 'task',
+        placeholder: 'ระบุข้อความ',
+        value: item.title
+      }],
+      buttons: [{
+        text: 'ยกเลิก', role: 'cancel'
+      },
+      {
+        text: 'แก้ไข',
+        handler: data => {
+          if (data.task !== '') {
+            item.title = data.task;
+          }
+          slidingItem.close();
+
+          return data.task;
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
 }
